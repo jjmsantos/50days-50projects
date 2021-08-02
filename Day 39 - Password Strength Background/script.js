371acc3d7bf6725c4d19f5a9e7e4c18ab6f20b7d
JSantos <jeremy.santos1010@gmail.com>
2021-08-01 23:40:40 -0400

    added day 38 & day 39

---
+++ Day 39 - Password Strength Background/script.js
@@ -0,0 +1,15 @@
+const password = document.getElementById("password");
+const background = document.getElementById("background");
+
+password.addEventListener("input", (e) => {
+	const input = e.target.value;
+	//console.log(input);
+
+	const length = input.length;
+	//console.log(length);
+
+	//console.log(20 - length * 2);
+
+	const blurValue = 20 - length * 2;
+	background.style.filter = `blur(${blurValue}px)`;
+});
